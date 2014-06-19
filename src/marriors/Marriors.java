6337b6e7479872238d4b6ddf1a3b35a1faff9cf1
kiavel <efrebault@outlook.com>
2014-06-19 09:12:25 +0200

    test

--- src/marriors/Marriors.java
+++ src/marriors/Marriors.java
@@ -17,7 +17,7 @@ public class Marriors {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("teste");
+        System.out.println("test");
     }
     
 }

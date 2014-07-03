9c8ff6fbc6f7b78d99aefc327d3356f956b081f1
kiavel <efrebault@outlook.com>
2014-07-03 11:25:33 +0200

    test

--- src/marriors/Marriors.java
+++ src/marriors/Marriors.java
@@ -5,6 +5,16 @@
  */
 
 package marriors;
+import View.*;
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.GraphicsEnvironment;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -17,9 +27,25 @@ public class Marriors {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Ceci est un test");
-        System.out.println("Ceci est un test de manu");
+        registerFont();
+        EntreeEnStock s = new  EntreeEnStock();
+        s.show();
     }
-    
-}
+        public static void registerFont() {
+                try{
+                        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+                        
+                        ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File(Paths.get(System.getProperty("user.dir"),"/src/Ressources/open-sans.bold.ttf").toUri())));
+                        ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File(Paths.get(System.getProperty("user.dir"),"/src/Ressources/open-sans.italic.ttf").toUri())));
+                        ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File(Paths.get(System.getProperty("user.dir"),"/src/Ressources/open-sans.regular.ttf").toUri())));
+                }
+                catch(IOException ex ){
+                        JOptionPane.showMessageDialog(null,ex.getMessage());
+                } catch (FontFormatException ex) {
+                        Logger.getLogger(Marriors.class.getName()).log(Level.SEVERE, null, ex);
+                 }
+
+        }
+    }
+
 

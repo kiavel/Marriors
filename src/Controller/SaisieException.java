14cce52819a0abc7460f744cfeb29758d7f9b926
xakan <faliere.benjamin@gmail.com>
2014-07-03 14:48:31 +0200

    Merge origin/master
    Conflicts:
    	.gitignore
    	src/marriors/Marriors.java

---
+++ src/Controller/SaisieException.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import javax.swing.JOptionPane;
+
+/**
+ * Classe d'exception personnalisé qui permet de faire remonter certaines exceptions 
+ * jusqu'à la couche interface afin de les indiquer à l'utilisateur
+ * @author mariebarbier
+ */
+public class SaisieException  extends Exception {
+    /**
+     * titre a afficher dans la boite de dialogue
+     */
+    private String dialogTitre;
+    /**
+     * message a afficher dans la boite de dialogue
+     */
+    private String dialogMessage;
+    /**
+     * type de boite de dialogue conformément aux options de JOptionPane
+     */
+    private int dialogOption;
+
+    /**
+     * 
+     * @param dialogTitre titre a afficher dans la boite de dialogue
+     * @param dialogMessage message a afficher dans la boite de dialogue
+     * @param dialogOption  type de boite de dialogue
+     */
+    public SaisieException(String dialogTitre, String dialogMessage, int dialogOption) {
+        this.dialogTitre = dialogTitre;
+        this.dialogMessage = dialogMessage;
+        this.dialogOption = dialogOption;
+    }
+
+    public String getDialogTitre() {
+        return dialogTitre;
+    }
+
+    public String getDialogMessage() {
+        return dialogMessage;
+    }
+
+    public int getDialogOption() {
+        return dialogOption;
+    }
+     
+     public String toString() {
+        return dialogTitre+" : "+dialogMessage;
+  }
+}
